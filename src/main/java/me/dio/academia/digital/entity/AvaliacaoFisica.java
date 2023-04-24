0c679d2538d817e467caef02a2e677c81a4219e5
GleidsonNV <111028199+GleidsonNV@users.noreply.github.com>
2023-04-24 00:58:42 -0300

    Atividade finalizada

---
+++ src/main/java/me/dio/academia/digital/entity/AvaliacaoFisica.java
@@ -0,0 +1,36 @@
+package me.dio.academia.digital.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class AvaliacaoFisica {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "aluno_id")
+  private Aluno aluno;
+
+  private LocalDateTime dataDaAvaliacao = LocalDateTime.now();
+
+  private double peso;
+
+  private double altura;
+
+}
